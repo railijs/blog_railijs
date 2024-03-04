4c1201d8e83be1e07c4316dcb752e402cd52a28a
railijs <railijsgrieznis@gmail.com>
2024-03-04 15:19:16 +0200

    gang

--- Database.php
+++ Database.php
@@ -10,7 +10,6 @@ class Database {
         
 
         $connection_string = "mysql:" . http_build_query($config, "", ";");
-        // $connection_string = "mysql:host=$config[host];dbname=$config[dbname];user=$config[user];password=$config[password  ];charset=$config[charset]";
         $this->pdo = new PDO($connection_string);
         $this->pdo -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
     }
--- index.php
+++ index.php
@@ -3,16 +3,29 @@ require "functions.php";
 require "Database.php";
 $config = require ("config.php");
 
- 
+
+$query = "SELECT * FROM posts";
+if (isset($_GET["id"]) && $_GET["id"] != "") {
+$id = $_GET["id"];
+$query = "SELECT * FROM posts WHERE id = $id";
+}
 
 $db = new Database($config);
-$posts = $db -> execute("SELECT * FROM posts") 
+$posts = $db -> execute($query) 
              -> fetchAll();
 
 
-echo "<ol>";
+ echo "<form>";
+ echo "<input name='id'/>";
+ echo "<button>Submit</button>";
+ echo "</form>";
+
+
+ echo "<h1>Posts</h1>";
+
+echo "<ul>";
 foreach($posts as $post) {
     echo "<li>" . $post["title"] . "</li>";
 }
-echo "</ol>";
+echo "</ul>";
 ?>
